042f0e39a211730a5c09f3a1a1b77c12484bebc7
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-08-04 12:24:36 +0600

    disabled dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
-    darkMode: ["class"],
+    darkMode: [],
     theme: {
         container: {
             center: true,
